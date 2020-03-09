c2859768176232a16004fc8dd255001cac1412c5
Jamills <sdfgcvbm@gmail.com>
2020-03-09 22:01:39 +0200

    Update calculator_brain.dart

--- calculator_brain.dart
+++ calculator_brain.dart
@@ -15,21 +15,10 @@ class ClaculatorBrain {
 
   String getResult() {
     if (_bmi >= 25) {
-      return 'Owerveight';
+      return 'Owerweight';
     } else if (_bmi > 18.5) {
       return 'Normal';
     } else {
       return 'Underweight';
     }
   }
-
-  String getInterpretation() {
-    if (_bmi >= 25) {
-      return 'You have a higher than normal body weight. Try to exrecise more.';
-    } else if (_bmi > 18.5) {
-      return 'You have a normal body weight. Good job!';
-    } else {
-      return 'You have a lower than normal body weight. You can eat a bit more.';
-    }
-  }
-}
